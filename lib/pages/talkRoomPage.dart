d2abda3fbcc04e0d0532eb44c3a8d55db3373102
ryuji55 <rt.october.2@icloud.com>
2024-09-12 09:16:05 +0900

    メッセージの入力インプット作成

--- lib/pages/talkRoomPage.dart
+++ lib/pages/talkRoomPage.dart
@@ -73,44 +73,83 @@ class _TalkRoomPageState extends State<TalkRoomPage> {
         appBar: AppBar(
           title: Text(widget.name),
         ),
-        body: ListView.builder(
-          // shrinkWrap: true,
-          reverse: true,
-          itemCount: messageList.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.only(
-                  top: 10, left: 10, right: 10, bottom: index == 0 ? 10 : 0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                textDirection: messageList[index].isMe
-                    ? TextDirection.rtl
-                    : TextDirection.ltr,
-                children: [
-                  Container(
-                      decoration: BoxDecoration(
-                        color: messageList[index].isMe
-                            ? Colors.green
-                            : Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 5),
-                      constraints: BoxConstraints(
-                        maxWidth:
-                            MediaQuery.of(context).size.width * 0.6, //画面幅の60%
-                      ),
-                      child: Text(
-                        messageList[index].message,
-                        softWrap: true, // 自動的に改行
-                        maxLines: null, // 行数制限なしでテキストを表示
-                      )),
-                  Text(intl.DateFormat('HH:mm')
-                      .format(messageList[index].sendTime)),
-                ],
+        body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 60),
+              child: ListView.builder(
+                // shrinkWrap: true,
+                reverse: true,
+                itemCount: messageList.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                        top: 10,
+                        left: 10,
+                        right: 10,
+                        bottom: index == 0 ? 10 : 0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      textDirection: messageList[index].isMe
+                          ? TextDirection.rtl
+                          : TextDirection.ltr,
+                      children: [
+                        Container(
+                            decoration: BoxDecoration(
+                              color: messageList[index].isMe
+                                  ? Colors.green
+                                  : Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            constraints: BoxConstraints(
+                              maxWidth: MediaQuery.of(context).size.width *
+                                  0.6, //画面幅の60%
+                            ),
+                            child: Text(
+                              messageList[index].message,
+                              softWrap: true, // 自動的に改行
+                              maxLines: null, // 行数制限なしでテキストを表示
+                            )),
+                        Text(intl.DateFormat('HH:mm')
+                            .format(messageList[index].sendTime)),
+                      ],
+                    ),
+                  );
+                },
               ),
-            );
-          },
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 60,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 10),
+                    child: Row(
+                      children: [
+                        const Expanded(
+                            child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'メッセージを入力',
+                                    contentPadding: const EdgeInsets.all(10),
+                                    border: OutlineInputBorder()))),
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.send))
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).padding.bottom,
+                  color: Colors.lightBlueAccent,
+                )
+              ],
+            ),
+          ],
         ));
   }
 }
