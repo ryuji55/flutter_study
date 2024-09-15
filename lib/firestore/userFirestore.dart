806b6307ba681d0b8fa78a871c580d86101e25ef
ryuji55 <rt.october.2@icloud.com>
2024-09-15 09:35:11 +0900

    userからusersに修正

--- lib/firestore/userFirestore.dart
+++ lib/firestore/userFirestore.dart
@@ -3,11 +3,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class UserFirestore {
   static final FirebaseFirestore _firebaseFirestoreInstance =
       FirebaseFirestore.instance;
-  static final _userCollection = _firebaseFirestoreInstance.collection('users');
+  static final _usersCollection =
+      _firebaseFirestoreInstance.collection('users');
 
   static Future<void> createUser() async {
     try {
-      await _userCollection.add({
+      await _usersCollection.add({
         'name': 'ナナシさん',
         'imageUrl':
             'https://yt3.googleusercontent.com/ngVd2-zv5o3pGUCfiVdZXCHhnq_g1Lo1Y8DbrmB9O8G7DG0IWUQJgsacqsI_LRvZE8JTsbQIuQ=s900-c-k-c0x00ffffff-no-rj',
